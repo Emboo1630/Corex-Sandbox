5e57548864e4f11cfe1c06a101178ea810f18a25
Antonio Aradaza <antonio@quantumcrowd.io>
2019-08-13 10:41:52 +0800

    minor fixes

--- workflows/src/main/kotlin/corexchange/corexflows/ShareInfoFlow.kt
+++ workflows/src/main/kotlin/corexchange/corexflows/ShareInfoFlow.kt
@@ -19,28 +19,32 @@ class ShareInfoFlow (private val recipient: String): CorexFunctions()
     @Suspendable
     override fun call(): SignedTransaction
     {
-        val criteria = QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.ALL)
-        val x = serviceHub.vaultService.queryBy<UserState>(criteria).states
-        val y = serviceHub.vaultService.queryBy<UserState>(criteria).states.map {
-            it.state.data
-        }
-
-        val builder = TransactionBuilder(notary = getPreferredNotary(serviceHub)).apply {
-            for (itx in x)
-            {
-                addInputState(itx)
-            }
-
-            for (ity in y)
-            {
-                addOutputState(ity.copy(participants = listOf(ourIdentity, stringToParty(recipient))), UserContract.CONTRACT_ID)
-            }
-            addCommand(Command(UserContract.Commands.Move(), listOf(ourIdentity.owningKey, stringToParty(recipient).owningKey)))
-        }
-        val signedTransaction = verifyAndSign(builder)
+        val signedTransaction = verifyAndSign(shareInfo())
         val session = initiateFlow(stringToParty(recipient))
         val transactionSignedByParties = subFlow(CollectSignaturesFlow(signedTransaction, listOf(session)))
         return subFlow(FinalityFlow(transactionSignedByParties, listOf(session)))
+    }
+
+    private fun shareInfo() = TransactionBuilder(notary = getPreferredNotary(serviceHub)).apply {
+        val criteria = QueryCriteria.VaultQueryCriteria(status = Vault.StateStatus.ALL)
+        val listOfInputs = serviceHub.vaultService.queryBy<UserState>(criteria).states
+        val listOfOutputs = serviceHub.vaultService.queryBy<UserState>(criteria).states.map {
+            it.state.data
+        }
+
+        // List of user input states
+        for (itx in listOfInputs)
+        {
+            addInputState(itx)
+        }
+
+        // List of user states
+        for (ity in listOfOutputs)
+        {
+            addOutputState(ity.copy(participants = listOf(ourIdentity, stringToParty(recipient))), UserContract.CONTRACT_ID)
+        }
+
+        addCommand(Command(UserContract.Commands.Move(), listOf(ourIdentity.owningKey, stringToParty(recipient).owningKey)))
     }
 }
 
