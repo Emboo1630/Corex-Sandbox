df4e71e26e9d901925c657f815dce59e126ef256
Antonio Aradaza <antonio@quantumcrowd.io>
2019-08-14 15:37:58 +0800

    minor fix on user register flow

--- workflows/src/main/kotlin/corexchange/userflows/UserRegisterFlow.kt
+++ workflows/src/main/kotlin/corexchange/userflows/UserRegisterFlow.kt
@@ -3,10 +3,12 @@ package corexchange.userflows
 import co.paralleluniverse.fibers.Suspendable
 import com.r3.corda.lib.tokens.workflows.utilities.getPreferredNotary
 import com.r3.corda.lib.tokens.contracts.types.TokenType
+import com.r3.corda.lib.tokens.money.DigitalCurrency
 import com.r3.corda.lib.tokens.money.FiatCurrency
 import corexchange.*
 import corexchange.contracts.UserContract
 import corexchange.states.UserState
+import jdk.nashorn.internal.parser.Token
 import net.corda.core.contracts.Amount
 import net.corda.core.contracts.Command
 import net.corda.core.contracts.UniqueIdentifier
@@ -15,11 +17,15 @@ import net.corda.core.flows.FinalityFlow
 import net.corda.core.flows.StartableByRPC
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
+import org.apache.commons.lang.mutable.Mutable
+import org.hibernate.mapping.IndexedCollection
 import java.util.ArrayList
 
 @StartableByRPC
 class UserRegisterFlow (private val name: String,
-                        private val wallet: MutableList<Amount<TokenType>>): UserFunctions()
+                        private val amount: MutableList<Long>,
+                        private val currency: MutableList<String>,
+                        private val fractionDigits: MutableList<Int>): UserFunctions()
 {
     @Suspendable
     override fun call(): SignedTransaction
@@ -40,12 +46,25 @@ class UserRegisterFlow (private val name: String,
     {
         return UserState(
                 name = name,
-                wallet = wallet,
+                wallet = amountAndCurrencyToTokenType(),
                 linearId = UniqueIdentifier(),
                 participants = listOf(ourIdentity)
         )
     }
 
+    private fun amountAndCurrencyToTokenType(): MutableList<Amount<TokenType>>
+    {
+        var index = 0
+        val listOfAmountCurrencyAndFractionDigits = mutableListOf(Amount(amount[index], TokenType(tokenIdentifier = currency[index], fractionDigits = fractionDigits[index])))
+        val iterate = amount.map { it }.size
+        listOfAmountCurrencyAndFractionDigits.removeAt(index)
+        while(index != iterate)
+        {
+            listOfAmountCurrencyAndFractionDigits.add(Amount(amount[index],TokenType(tokenIdentifier = currency[index], fractionDigits = fractionDigits[index])))
+            index++
+        }
+        return listOfAmountCurrencyAndFractionDigits
+    }
     private fun register() = TransactionBuilder(notary = getPreferredNotary(serviceHub)).apply {
         val command = Command(UserContract.Commands.Register(), ourIdentity.owningKey)
         addOutputState(outState(), UserContract.CONTRACT_ID)
